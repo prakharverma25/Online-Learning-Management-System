8ac16fb65323d0339055e8650afdba12847d7090
prakharverma25 <Prakhar.24scse1180209@galgotiasuniversity.ac.in>
2025-11-26 08:58:45 +0530

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,44 @@
+create database universitymanagement;
+use universitymanagement;
+create table login(username varchar (30) , password varchar(30));
+select * from login;
+insert into login values('prakhar','12345');
+insert into login values('alice','123');
+
+create table teacher(name varchar(40),fname varchar(40), empID varchar(40) , dob varchar(40), address varchar(40), phone varchar(40), email varchar(40), class_X varchar(40), class_XII varchar(40), aadhar varchar(40), education varchar(40), department varchar(40));
+select * from teacher;
+
+create table student(name varchar(40),fname varchar(40), rollno varchar(40) , dob varchar(40), address varchar(40), phone varchar(40), email varchar(40), class_X varchar(40), class_XII varchar(40), aadhar varchar(40), course varchar(40), branch varchar(40));
+select * from student;
+create table studentleave(rollno varchar(20), date varchar(20), time varchar(20));
+select * from studentleave;
+
+create table teacherleave(empID varchar(20), date varchar(30), time varchar(20));
+select * from teacherleave;
+
+create table subject(rollno varchar(20), semester varchar(20), subj1 varchar(50), subj2 varchar(50), subj3 varchar(50), subj4 varchar(50), subj5 varchar(50));
+select * from subject;
+
+create table marks(rollno varchar(20), semester varchar(20), mark1 varchar(50), mark2 varchar(50), mark3 varchar(50), mark4 varchar(50), mark5 varchar(50));
+select * from marks;
+create table fee(course varchar(20), semester1 varchar(20), semester2 varchar(20), semester3 varchar(20), semester4 varchar(20), semester5 varchar(20), semester6 varchar(20), semester7 varchar(20), semester8 varchar(20));
+select * from fee;
+
+insert into fee values("BTech", "49000", "43000","43000","43000","43000","43000","43000","43000");
+
+insert into fee values("Bsc", "44000", "35000","35000","35000","35000","35000","","");
+
+insert into fee values("BCA", "39000", "34000","34000","34000","34000","34000","","");
+
+insert into fee values("MTech", "70000", "60000","60000","60000","","","","");
+
+insert into fee values("MSc", "44000", "45000","45000","45000","","","","");
+
+insert into fee values("MCA", "66000", "42000","42000","49000","","","","");
+
+insert into fee values("Bcom", "32000", "20000","20000","20000","20000","20000","","");
+
+insert into fee values("Mcom", "46000", "30000","30000","30000","","","","");
+
+create table feecollege(rollno varchar(20), course varchar(20), branch varchar(20), semester varchar(20), total varchar(20));
+select * from feecollege;
